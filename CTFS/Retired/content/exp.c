f2612495496c553cbc3788ad80dbcf675f267f1d
dkb4rb <jjuannca651@gmail.com>
2022-05-17 02:54:43 -0500

    Triying privilege escalation, but the bin dont function

--- CTFS/Retired/content/exp.c
+++ CTFS/Retired/content/exp.c
@@ -13,7 +13,13 @@ int main(void) {
 
     	read(STDIN_FILENO, cmd, sizeof(cmd));
 	cmd[-1] = 0;
+
+	/*Vemos que el buffer pude ser explotado,, pero como?*/
 	printf("%s", cmd);
+
+
+
         return 0;
 }
+
 
